1e93e1096f70be535c9537221763fe71d45a5fa4
Vitalya <vitalikt_22@mail.ru>
2024-10-18 15:39:51 +0400

    App func component to class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Game from "./components/Game/Game"
+import React, { Component } from "react"
 
-function App() {
-	return (
-		<div className="text-center">
-			<header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-white">
-				<Game />
-			</header>
-		</div>
-	)
+export default class App extends Component {
+	render() {
+		return (
+			<div className="text-center">
+				<header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-white">
+					<Game />
+				</header>
+			</div>
+		)
+	}
 }
-
-export default App
